58333b319454603f92b43b16ddb583b2bc7f893d
Arib Anwar <hiareeb@gmail.com>
2019-10-01 12:01:38 +0530

    The third Java Project. Actually this is First.

---
+++ EclipseJavaProjectMaven/src/main/java/com/zensar/myjava/App.java
@@ -0,0 +1,13 @@
+package com.zensar.myjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Death is a Blessing bestowed upon us to save us from the curse of Life" );
+    }
+}
