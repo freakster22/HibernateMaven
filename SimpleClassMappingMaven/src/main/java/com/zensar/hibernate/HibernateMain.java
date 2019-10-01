7b7eba1074ddaa63d1908648700464870152802f
Arib Anwar <hiareeb@gmail.com>
2019-10-01 11:38:54 +0530

    Added a Print Statement in Hibernate Main class.

--- SimpleClassMappingMaven/src/main/java/com/zensar/hibernate/HibernateMain.java
+++ SimpleClassMappingMaven/src/main/java/com/zensar/hibernate/HibernateMain.java
@@ -7,33 +7,30 @@ import org.hibernate.cfg.Configuration;
 
 import com.zensar.hibernate.entities.Product;
 
-import org.hibernate.Session;
-
-
 /**
  * Hibernate Main
  *
  */
-public class HibernateMain 
-{
-    public static void main( String[] args )
-    {
-        //System.out.println( "This is a Hibernate Main File" );
-    	
-    	Configuration configuration = new Configuration().configure();
-    	SessionFactory factory = configuration.buildSessionFactory();
-    	Session session = factory.openSession();
-    	Transaction transaction = session.beginTransaction();
-    	
-    	Product product = new Product();
-    	product.setProductId(1043);
-    	product.setName("OP7T");
-    	product.setBrand("One Plus");
-    	product.setPrice(37000f);
-    	session.save(product);
-    	
-    	transaction.commit();
-    	session.close();
-    	
-    }
+public class HibernateMain {
+	public static void main(String[] args) {
+
+		Configuration configuration = new Configuration().configure();
+		SessionFactory factory = configuration.buildSessionFactory();
+		Session session = factory.openSession();
+		Transaction transaction = session.beginTransaction();
+
+		Product product = new Product();
+		product.setProductId(1043);
+		product.setName("OP7T");
+		product.setBrand("One Plus");
+		product.setPrice(37000f);
+
+		session.save(product);
+
+		System.out.println("Product is Saved here");
+
+		transaction.commit();
+		session.close();
+
+	}
 }
